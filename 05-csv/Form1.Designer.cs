ffdd0feb33d0fb1cf232f57607fcd6e0703aca7d
Martinvv <martinviso89@gmail.com>
2025-10-16 10:05:30 +0200

    Proyecto Nuevo CSV

---
+++ 05-csv/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace _05_csv
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            LeerCSVBtn = new Label();
+            btnLeerCSVHelper = new Button();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(46, 24);
+            button1.Name = "button1";
+            button1.Size = new Size(148, 40);
+            button1.TabIndex = 0;
+            button1.Text = "Leer CSV";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // LeerCSVBtn
+            // 
+            LeerCSVBtn.AutoSize = true;
+            LeerCSVBtn.Location = new Point(240, 37);
+            LeerCSVBtn.Name = "LeerCSVBtn";
+            LeerCSVBtn.Size = new Size(0, 15);
+            LeerCSVBtn.TabIndex = 1;
+            // 
+            // btnLeerCSVHelper
+            // 
+            btnLeerCSVHelper.Location = new Point(46, 70);
+            btnLeerCSVHelper.Name = "btnLeerCSVHelper";
+            btnLeerCSVHelper.Size = new Size(148, 40);
+            btnLeerCSVHelper.TabIndex = 2;
+            btnLeerCSVHelper.Text = "Leer CSV Helper";
+            btnLeerCSVHelper.UseVisualStyleBackColor = true;
+            btnLeerCSVHelper.Click += btnLeerCSVHelper_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnLeerCSVHelper);
+            Controls.Add(LeerCSVBtn);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Label LeerCSVBtn;
+        private Button btnLeerCSVHelper;
+    }
+}
---
+++ 05-csv/Form1.cs
@@ -0,0 +1,68 @@
+using CsvHelper;
+using CsvHelper.Configuration;
+using System.Globalization;
+
+namespace _05_csv
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string fichero = "personas_punto_coma.csv";
+            if (File.Exists(fichero))
+            {
+                using (StreamReader sr = new StreamReader(fichero))
+                {
+                    string? linea;
+                    while ((linea = sr.ReadLine()) != null)
+                    {
+                        //dividir por punto y coma
+                        var campos = linea.Split(";");
+
+                        foreach (var campo in campos)
+                        {
+                            this.LeerCSVBtn.Text += campo + " ";
+                        }
+                        this.LeerCSVBtn.Text += System.Environment.NewLine;
+                    }
+                }
+            }
+            else
+            {
+
+            }
+        }
+
+        private void btnLeerCSVHelper_Click(object sender, EventArgs e)
+        {
+            string fichero = "personas.csv";
+            var config = new CsvConfiguration(CultureInfo.InvariantCulture)
+            {
+
+            };
+
+            StreamReader sr = new StreamReader(fichero);
+            using (var csv = new CsvReader(sr , config))
+            {
+                var personas = csv.GetRecords<Persona>();
+
+                foreach (var persona in personas)
+                {
+                    this.LeerCSVBtn.Text    += persona.nombre + " " 
+                                            + persona.edad
+                                            + " " + persona.altura + System.Environment.NewLine;
+                }
+            }
+        }
+    }
+}
