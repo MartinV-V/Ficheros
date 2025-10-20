12adcdccfe63432cc9fb3b20d5da6689e237acd2
Martinvv <martinviso89@gmail.com>
2025-10-20 14:51:19 +0200

    ejercicio json

--- 06-Json/Form1.Designer.cs
+++ 06-Json/Form1.Designer.cs
@@ -29,34 +29,131 @@
         private void InitializeComponent()
         {
             btnLeerJsonLocal = new Button();
-            JsonLbl = new Label();
+            lblResultadoFichero = new Label();
+            txtNombre = new TextBox();
+            txtEdad = new TextBox();
+            txtProfesion = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            button1 = new Button();
+            btnAppendPersona = new Button();
+            button2 = new Button();
             SuspendLayout();
             // 
             // btnLeerJsonLocal
             // 
-            btnLeerJsonLocal.Location = new Point(42, 50);
+            btnLeerJsonLocal.Location = new Point(340, 235);
             btnLeerJsonLocal.Name = "btnLeerJsonLocal";
             btnLeerJsonLocal.Size = new Size(205, 23);
             btnLeerJsonLocal.TabIndex = 0;
-            btnLeerJsonLocal.Text = "Leer personas.json";
+            btnLeerJsonLocal.Text = "Guardar json";
             btnLeerJsonLocal.UseVisualStyleBackColor = true;
-            btnLeerJsonLocal.Click += btnLeerJsonLocal_Click;
+            btnLeerJsonLocal.Click += btnSerializarFromForm_Click;
             // 
-            // JsonLbl
+            // lblResultadoFichero
             // 
-            JsonLbl.AutoSize = true;
-            JsonLbl.Location = new Point(297, 54);
-            JsonLbl.Name = "JsonLbl";
-            JsonLbl.Size = new Size(13, 15);
-            JsonLbl.TabIndex = 1;
-            JsonLbl.Text = "::";
+            lblResultadoFichero.AutoSize = true;
+            lblResultadoFichero.Location = new Point(297, 54);
+            lblResultadoFichero.Name = "lblResultadoFichero";
+            lblResultadoFichero.Size = new Size(13, 15);
+            lblResultadoFichero.TabIndex = 1;
+            lblResultadoFichero.Text = "::";
+            // 
+            // txtNombre
+            // 
+            txtNombre.Location = new Point(170, 163);
+            txtNombre.Name = "txtNombre";
+            txtNombre.Size = new Size(100, 23);
+            txtNombre.TabIndex = 2;
+            // 
+            // txtEdad
+            // 
+            txtEdad.Location = new Point(170, 202);
+            txtEdad.Name = "txtEdad";
+            txtEdad.Size = new Size(100, 23);
+            txtEdad.TabIndex = 3;
+            // 
+            // txtProfesion
+            // 
+            txtProfesion.Location = new Point(170, 240);
+            txtProfesion.Name = "txtProfesion";
+            txtProfesion.Size = new Size(100, 23);
+            txtProfesion.TabIndex = 4;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(95, 166);
+            label1.Name = "label1";
+            label1.Size = new Size(51, 15);
+            label1.TabIndex = 5;
+            label1.Text = "Nombre";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(95, 202);
+            label2.Name = "label2";
+            label2.Size = new Size(33, 15);
+            label2.TabIndex = 6;
+            label2.Text = "Edad";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(95, 243);
+            label3.Name = "label3";
+            label3.Size = new Size(57, 15);
+            label3.TabIndex = 7;
+            label3.Text = "Profesión";
+            label3.Click += label3_Click;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(56, 50);
+            button1.Name = "button1";
+            button1.Size = new Size(205, 23);
+            button1.TabIndex = 8;
+            button1.Text = "Guardar persona.json";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += btnSerializarAFichero_Click;
+            // 
+            // btnAppendPersona
+            // 
+            btnAppendPersona.Location = new Point(340, 166);
+            btnAppendPersona.Name = "btnAppendPersona";
+            btnAppendPersona.Size = new Size(205, 23);
+            btnAppendPersona.TabIndex = 9;
+            btnAppendPersona.Text = "Append Persona";
+            btnAppendPersona.UseVisualStyleBackColor = true;
+            btnAppendPersona.Click += btnAppendPersona_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(56, 79);
+            button2.Name = "button2";
+            button2.Size = new Size(205, 23);
+            button2.TabIndex = 10;
+            button2.Text = "Leer json";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += btnLeerJsonLocal_Click;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(355, 144);
-            Controls.Add(JsonLbl);
+            ClientSize = new Size(758, 375);
+            Controls.Add(button2);
+            Controls.Add(btnAppendPersona);
+            Controls.Add(button1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(txtProfesion);
+            Controls.Add(txtEdad);
+            Controls.Add(txtNombre);
+            Controls.Add(lblResultadoFichero);
             Controls.Add(btnLeerJsonLocal);
             Name = "Form1";
             Text = "Form1";
@@ -67,6 +164,15 @@
         #endregion
 
         private Button btnLeerJsonLocal;
-        private Label JsonLbl;
+        private Label lblResultadoFichero;
+        private TextBox txtNombre;
+        private TextBox txtEdad;
+        private TextBox txtProfesion;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Button button1;
+        private Button btnAppendPersona;
+        private Button button2;
     }
 }
--- 06-Json/Form1.cs
+++ 06-Json/Form1.cs
@@ -1,13 +1,8 @@
+using _06_json;
 using System.Text.Json;
 
 namespace _06_Json
 {
-    public class Persona
-    {
-        public string nombre { get; set; }
-        public int edad { get; set; }
-        public string profesion { get; set; }
-    }
     public partial class Form1 : Form
     {
         public Form1()
@@ -17,20 +12,93 @@ namespace _06_Json
 
         private void btnLeerJsonLocal_Click(object sender, EventArgs e)
         {
-            string? ficheroJson = "persona.json";
+            string ficheroJSON = "persona.json";
 
-            if (File.Exists(ficheroJson))
+            if (File.Exists(ficheroJSON))
             {
-                string jsonText = File.ReadAllText(ficheroJson);
-                Persona? p = JsonSerializer.Deserialize<Persona>(jsonText);
+                string jsonText = File.ReadAllText(ficheroJSON);
+                Persona p = JsonSerializer.Deserialize<Persona>(jsonText);
 
-                this.JsonLbl.Text += "Nombre: " + p.nombre + "\n" + "Edad: " + p.edad
-                    + "\n" + "Profesion: " + p.profesion;
+                this.lblResultadoFichero.Text += "Nombre: " + p.nombre + System.Environment.NewLine +
+                "Edad: " + p.edad + System.Environment.NewLine +
+                "Porfesion" + p.profesion + System.Environment.NewLine;
+
+
             }
             else
             {
-                this.JsonLbl.Text = "No existe el fichero pillin";
+                this.lblResultadoFichero.Text = "El fichero no existe";
             }
+
+        }
+
+        private void btnSerializarAFichero_Click(object sender, EventArgs e)
+        {
+            Persona p = new Persona
+            {
+                nombre = "Macarena",
+                edad = 19,
+                profesion = "Desarrolladora python"
+            };
+
+            string jsonText = JsonSerializer.Serialize(p, new JsonSerializerOptions { WriteIndented = true });
+            File.WriteAllText("persona_guardada.json", jsonText);
+
+            this.lblResultadoFichero.Text = "Archivo JSON guardado correctamente.";
+        }
+
+        private void btnSerializarFromForm_Click(object sender, EventArgs e)
+        {
+            Persona p = new Persona();
+            p.nombre = this.txtNombre.Text;
+            p.edad = System.Convert.ToInt32(this.txtEdad.Text);
+            p.profesion = this.txtProfesion.Text;
+
+            string jsonText = JsonSerializer.Serialize(p, new JsonSerializerOptions { WriteIndented = true });
+            File.WriteAllText("persona_guardada_form.json", jsonText);
+
+            File.WriteAllText("persona_guardada_form.json", jsonText);
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAppendPersona_Click(object sender, EventArgs e)
+        {
+            string? jsonFich = "persona.json";
+            List<Persona> personas = new List<Persona>();
+
+            // Verificar si el archivo JSON existe
+            if (File.Exists(jsonFich))
+            {
+                // Leer el contenido del archivo JSON
+                string jsonText = File.ReadAllText(jsonFich);
+
+                // Deserializar el contenido como una lista de personas
+                personas = JsonSerializer.Deserialize<List<Persona>>(jsonText) ?? new List<Persona>();
+            }
+
+            // Crear una nueva persona con los datos de los TextBox
+            Persona nuevaPersona = new Persona
+            {
+                nombre = txtNombre.Text,
+                edad = System.Convert.ToInt32(this.txtEdad.Text),
+                profesion = this.txtProfesion.Text
+            };
+
+            // Agregar la nueva persona a la lista
+            personas.Add(nuevaPersona);
+
+            // Serializar la lista actualizada y guardar en el archivo JSON
+            string jsonText2 = JsonSerializer.Serialize(personas, new JsonSerializerOptions { WriteIndented = true });
+            File.WriteAllText(jsonFich, jsonText2);
+
+            // Actualizar el resultado en la interfaz
+            this.lblResultadoFichero.Text = "Archivo JSON actualizado correctamente.";
         }
     }
 }
+
